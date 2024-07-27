9d77da24b7389fd1605cc1c2055568ee6fc43c12
mayuresh1301 <62095050+mayuresh1301@users.noreply.github.com>
2024-07-27 15:47:52 -0400

    Updated the Videos and Photos components and added styling

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import './Photos.css';
 
 const Photos = () => {
+    const [photos, setPhotos] = useState([]);
+    const [category, setCategory] = useState("");
+
+    useEffect(() => {
+        fetchPhotos();
+    }, [category]);
+
+    const fetchPhotos = async () => {
+        const response = await fetch("https://jsonplaceholder.typicode.com/photos");
+        const data = await response.json();
+        setPhotos(data.slice(0, 10));
+    };
+
+
     return (
-        <div>
+        <div className="photos">
+
             <h1>Photos</h1>
-            <p>Photo gallery with category filters.</p>
+            <select onChange={(e) => setCategory(e.target.value)} value={category}>
+                <option value="">ALL</option>
+                <option value="events">Events</option>
+                <option value="weddings">Weddings</option>
+            </select>
+            <div className="gallery">
+                {photos.map((photo) => (
+                    <div key={photo.id} className="photo-item">
+                        <img src={photo.thumbnailUrl} alt={photo.title} />
+                        <p>{photo.title}</p>
+                    </div>
+                ))}
+            </div>
+                
+            
         </div>
     );
 };
--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import './Videos.css';
 
 const Videos = () => {
+    const [videos, setVideos] = useState([]);
+    const [category, setCategory] = useState("");
+
+    useEffect(() => {
+        fetchVideos();
+    }, [category]);
+
+    const fetchVideos = async () => {
+        const response = await fetch("https://jsonplaceholder.typicode.com/photos");
+        const data = await response.json(); 
+        //console.log(data);
+        setVideos(data.slice(0, 10));
+    };
+
     return (
-        <div>
+        <div className="videos">
             <h1>Videos</h1>
-            <p>Video gallery with category filters.</p>
+            <select onChange={(e) => setCategory(e.target.value)} value = {category}>
+                <option value = "">ALL</option>
+                <option value = "events">Events</option>
+                <option value = "weddings">Weddings</option>
+            </select>
+            <div className="gallery">
+                {videos.map(video => (
+                    <div key = {video.id} className="video-item">
+                        <img src = {video.thumbnailUrl} alt = {video.title} />
+                        <p>{video.title}</p>
+                    </div>
+                ))}
+            </div>
+            
         </div>
     );
 };
