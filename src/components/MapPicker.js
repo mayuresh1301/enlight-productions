016ec8fdd6062d70fb9881b370fd048d19a8db14
mayuresh1301 <62095050+mayuresh1301@users.noreply.github.com>
2024-10-22 17:38:20 -0400

    Add search functionality to MapPicker and update InquiryForm

--- src/components/MapPicker.js
+++ src/components/MapPicker.js
@@ -1,7 +1,10 @@
-import React, { useState } from "react";
-import { MapContainer, TileLayer, Marker, useMapEvents } from 'react-leaflet';
+import React, { useState, useEffect } from "react";
+import { MapContainer, TileLayer, Marker, useMapEvents, useMap } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
+import "leaflet-control-geocoder/dist/Control.Geocoder.css";
+import "leaflet-control-geocoder";
+import { query } from "firebase/firestore";
 
 delete L.Icon.Default.prototype._getIconUrl;
 
@@ -28,6 +31,35 @@ const MapPicker = ({ onLocationSelect, defaultPosition = { lat: 40.73061, lng: -
         return null;
     }
 
+    const Geocoder = () => {
+        const map = useMap();
+
+        useEffect(() => {
+            let geocoder = L.Control.Geocoder.nominatim();
+
+            const control = L.Control.geocoder({
+                query: "",
+                placeholder: "Search for a location",
+                defaultMarkGeocode: false,
+                geocoder, 
+            })
+            .on('markgeocode', function(e) {
+                const latlng = e.geocode.center;
+                map.flyTo(latlng, 13);
+                setMarkerPosition(latlng);
+                onLocationSelect(latlng);
+                //map.setView(latlng, 13);
+            })
+            .addTo(map);
+
+            return () => {
+                map.removeControl(control);
+            }
+        }, [map]);
+
+        return null;
+    }
+
     return (
         <MapContainer
             center={markerPosition}
@@ -38,6 +70,7 @@ const MapPicker = ({ onLocationSelect, defaultPosition = { lat: 40.73061, lng: -
                 attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
+            <Geocoder />
             <MapEvents />
             <Marker position={markerPosition} />
         </MapContainer>
